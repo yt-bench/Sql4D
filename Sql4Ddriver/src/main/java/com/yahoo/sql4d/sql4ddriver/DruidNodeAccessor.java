f784668bf80e54c0a9d2b823e796d49cf5ca3f69
搏远 <yangtao.yt@alibaba-inc.com>
2016-08-16 17:51:50 +0800

    Fix request error(400 Illegal character 0x0) when druid-broker use jetty9.3+ , solution: do not set json to header, looks like useless.

--- Sql4Ddriver/src/main/java/com/yahoo/sql4d/sql4ddriver/DruidNodeAccessor.java
+++ Sql4Ddriver/src/main/java/com/yahoo/sql4d/sql4ddriver/DruidNodeAccessor.java
@@ -135,7 +135,7 @@ public class DruidNodeAccessor {
         CloseableHttpResponse resp = null;
         HttpPost post = new HttpPost(url);
         addHeaders(post, reqHeaders);
-        post.setHeader(json, url);
+//        post.setHeader(json, url);
         StringEntity input = new StringEntity(json, ContentType.APPLICATION_JSON);
         post.setEntity(input);
         resp = req.execute(post);
